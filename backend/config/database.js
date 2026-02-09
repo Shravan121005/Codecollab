8e2bea17364bc2685ef38f92a68b7409a8d16b0a
Shravan Jain <shravan.jain1210@gmail.com>
2026-02-09 14:06:22 +0530

    some changes

--- backend/config/database.js
+++ backend/config/database.js
@@ -7,33 +7,33 @@ let sequelize;
 
 // for prod use to connect to render postgres
 
-// if (process.env.DATABASE_URL) {
-//   sequelize = new Sequelize(process.env.DB_URL, {
-//     dialect: 'postgres',
-//     protocol: 'postgres',
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false
-//       }
-//     },
-//     logging: false
-//   });
-// } 
-// // Fallback for local docker-compose
-// else {
-//   sequelize = new Sequelize(
-//     process.env.DB_NAME,
-//     process.env.DB_USER,
-//     process.env.DB_PASSWORD,
-//     {
-//       host: process.env.DB_HOST,
-//       port: process.env.DB_PORT,
-//       dialect: 'postgres',
-//       logging: false,
-//     }
-//   );
-// }
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DB_URL, {
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
+    logging: false
+  });
+} 
+// Fallback for local docker-compose
+else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+      logging: false,
+    }
+  );
+}
 
 // for local use to connect to db container
   sequelize = new Sequelize(
